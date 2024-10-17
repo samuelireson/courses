56f6d14fbbc71e2fe38dd1518b7c39e570894c29
Samuel Ireson <samuelireson@gmail.com>
2024-10-17 23:30:03 +0100

    implement course compilation

--- cli/cmd/convert.go
+++ cli/cmd/convert.go
@@ -33,10 +33,10 @@ var basicRegexPatterns = []regexPattern{
 	{regexp.MustCompile(`\\subsection\{(.*?)\}`), "### $1"},
 
 	// theorem environments
-	{regexp.MustCompile(`\\begin\{definition\}`), "<Aside type='definition' title='Definition' >"},
-	{regexp.MustCompile(`\\begin\{(theorem|lemma|proposition|corollary)\}`), "<Aside type='result' title='" + strings.ToUpper("$1") + "' >"},
-	{regexp.MustCompile(`\\begin\{(example|nonexample)\}`), "<Aside type='result' title='" + strings.ToUpper("$1") + "' >"},
-	{regexp.MustCompile(`\\begin\{(notation|remark)\}`), "<Aside type='result' title='" + strings.ToUpper("$1") + "' >"},
+	{regexp.MustCompile(`\\begin\{definition\}`), "<Aside type='definition' title='definition' >"},
+	{regexp.MustCompile(`\\begin\{(theorem|lemma|proposition|corollary)\}`), "<Aside type='result' title='$1' >"},
+	{regexp.MustCompile(`\\begin\{(example|nonexample)\}`), "<Aside type='example' title='$1' >"},
+	{regexp.MustCompile(`\\begin\{(notation|remark)\}`), "<Aside type='comment' title='$1' >"},
 	{regexp.MustCompile(`\\end\{(definition|theorem|lemma|proposition|corollary|example|nonexample|notation|remark)\}`), "</Aside>"},
 
 	// maths environments
@@ -54,19 +54,11 @@ var basicRegexPatterns = []regexPattern{
 	{regexp.MustCompile(`\\basic`), ":badge[Basic]{variant=success}"},
 	{regexp.MustCompile(`\\intermediate`), ":badge[Intermediate]{variant=warning}"},
 	{regexp.MustCompile(`\\challenging`), ":badge[Challenging]{variant=danger}"},
-}
 
-// convertCmd represents the convert command
-var convertCmd = &cobra.Command{
-	Use:   "convert",
-	Short: "Convert course notes from .tex to .mdx",
-	Long: `Convert LaTeX notes for a course to MarkdownX format, which can be
-	rendered on the web.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		inputPath := args[0]
-		outputPath := generateOutputFilePath(inputPath)
-		convertTeXtoMDX(inputPath, outputPath)
-	},
+	// fonts and ligatures
+	{regexp.MustCompile(`\\textbf\{(.*?)\}`), "* $1 *"},
+	{regexp.MustCompile(`\\textit\{(.*?)\}`), "** $1 **"},
+	{regexp.MustCompile("`"), "'"},
 }
 
 func generateOutputFilePath(input string) string {
@@ -80,32 +72,63 @@ func generateOutputFilePath(input string) string {
 	return outputPath
 }
 
-func convertTeXtoMDX(inputPath string, outputPath string) {
-	content, err := os.ReadFile(inputPath)
+func convertTeXtoMDX(content []byte) []byte {
+	for _, element := range basicRegexPatterns {
+		content = element.captureGroup.ReplaceAll(content, []byte(element.replacement))
+	}
+	return content
+}
+
+func processFile(inputPath string) {
+	outputPath := generateOutputFilePath(inputPath)
+	fi, err := os.ReadFile(inputPath)
 	if err != nil {
 		panic(err)
 	}
 
-	for _, element := range basicRegexPatterns {
-		content = element.captureGroup.ReplaceAll(content, []byte(element.replacement))
-	}
+	fo := convertTeXtoMDX(fi)
 
-	err = os.WriteFile(outputPath, content, 0644)
+	err = os.WriteFile(outputPath, fo, 0644)
 	if err != nil {
 		panic(err)
 	}
 }
 
+func processDir(dirPath string) {
+	files, err := os.ReadDir(dirPath)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, file := range files {
+		inputPath := filepath.Join(dirPath, file.Name())
+		processFile(inputPath)
+	}
+}
+
+var convertCmd = &cobra.Command{
+	Use:   "convert",
+	Short: "Convert course notes from .tex to .mdx",
+	Long: `Convert LaTeX notes for a course to MarkdownX format, which can be
+	rendered on the web.`,
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		if chapter {
+			processFile(args[0])
+		} else if course {
+			dirPath := filepath.Join(args[0], "/chapters")
+			processDir(dirPath)
+		}
+	},
+}
+
+var chapter bool
+var course bool
+
 func init() {
 	rootCmd.AddCommand(convertCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// convertCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// convertCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	convertCmd.Flags().BoolVarP(&chapter, "chapter", "c", false, "Chapter you want to convert")
+	convertCmd.Flags().BoolVarP(&course, "course", "C", false, "Course you want to convert")
+	convertCmd.MarkFlagsOneRequired("chapter", "course")
+	convertCmd.MarkFlagsMutuallyExclusive("chapter", "course")
 }
