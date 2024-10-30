9c756096ae153a03f35981879984304bf83bf7b7
Samuel Ireson <samuelireson@gmail.com>
2024-10-30 11:41:54 +0000

    download path final fix

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -9,6 +9,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://samuelireson.github.io/courses',
+	base: 'courses',
 	integrations: [starlight({
 		plugins: [starlightHeadingBadgesPlugin()],
 		title: 'Courses',
