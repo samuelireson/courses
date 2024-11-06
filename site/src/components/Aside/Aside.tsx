b5d917d62a09c71eedbc40bf14c47df264fc0d90
Samuel Ireson <samuelireson@gmail.com>
2024-11-06 13:55:21 +0000

    alter Aside to include naming, update learning-problems chapter

--- site/src/components/Aside/Aside.tsx
+++ site/src/components/Aside/Aside.tsx
@@ -6,14 +6,16 @@ const asideVariants = ['comment', 'definition', 'result', 'example'] as const;
 interface Props {
 	type: (typeof asideVariants)[number];
 	title: string;
+	name?: string,
 	children: React.ReactNode;
 }
 
-const Aside = ({ type, title, children }: Props) => {
+const Aside = ({ name, type, title, children }: Props) => {
 	return (
 		<aside aria-label={title} className={`starlight-aside starlight-aside--${type}`}>
 			<p className="starlight-aside__title" aria-hidden="true">
 				{title}
+				{(name != '') && <span>{name}</span>}
 			</p>
 			<section className="starlight-aside__content">
 				{children}
